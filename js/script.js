8595f3a614cae903824956587831fb10fc927327
cameronmcnz <you@example.com>
2023-10-06 18:43:36 +0300

    first COMMIT

---
+++ js/script.js
@@ -0,0 +1,64 @@
+
+console.log("ghj");
+console.log("ghj");
+console.log("ghj");
+
+const header = document.querySelector('header');
+window.addEventListener('scroll',function(){
+header.classList.toggle('stickyy' , window.scrollY > 100);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let icon=document.getElementById('menu-icon')
+let bar=document.getElementById('barr')
+icon.onclick=function(){
+    bar.classList.toggle('non')
+}*/
